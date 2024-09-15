200218a37f2e2489fc1be2fd1958014e99322a18
Leah Deshar <leah.desar@gmail.com>
2024-09-15 12:32:32 +0545

    home page settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,15 +22,11 @@ const App = () => {
 };
 
 const RootLayout = () => {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 };
 
