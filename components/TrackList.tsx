e88a186b0d0e6fd3615aaf4c1cdcd71e057a7e8d
Leah Deshar <leah.desar@gmail.com>
2024-09-16 17:11:00 +0545

    all screen layout set

---
+++ components/TrackList.tsx
@@ -0,0 +1,8 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+
+const TrackList = () => {
+  return <FlatList data={[]} renderItem={() => null} />;
+};
+
+export default TrackList;
