ca5bc4947fd52e20ed4d2b8e7d80d1072f4cc9ca
Leah Deshar <leah.desar@gmail.com>
2024-09-17 15:59:47 +0545

    tracklist

---
+++ components/TrackListItem.tsx
@@ -0,0 +1,14 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const TrackListItem = () => {
+  return (
+    <TouchableOpacity>
+      <View>
+        <FastImage />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default TrackListItem;
