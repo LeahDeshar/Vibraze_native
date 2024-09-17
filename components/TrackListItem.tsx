808682fb15bca216437dbb5b62ea90b40779bd70
Leah Deshar <leah.desar@gmail.com>
2024-09-17 16:07:11 +0545

    tracklist

--- components/TrackListItem.tsx
+++ components/TrackListItem.tsx
@@ -1,11 +1,12 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import FastImage from "react-native-fast-image";
 
 const TrackListItem = () => {
   return (
     <TouchableOpacity>
       <View>
-        <FastImage />
+        <FastImage source={{}} />
       </View>
     </TouchableOpacity>
   );
