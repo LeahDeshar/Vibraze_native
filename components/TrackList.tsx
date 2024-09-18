6f746843ec587b4a0ccfc32843bdfc71c3c2c8ff
Leah Deshar <leah.desar@gmail.com>
2024-09-18 16:19:29 +0545

    bug fix

--- components/TrackList.tsx
+++ components/TrackList.tsx
@@ -1,12 +1,20 @@
 import { View, Text, FlatList, FlatListProps } from "react-native";
 import library from "@/assets/data/library.json";
 import TrackListItem from "./TrackListItem";
+import { utilStyles } from "@/styles";
 
 export type TrackListProps = Partial<FlatListProps<unknown>>;
+
+const ItemDivider = () => (
+  <View
+    style={{ ...utilStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }}
+  />
+);
 const TrackList = ({ ...flatlistProps }: TrackListProps) => {
   return (
     <FlatList
       data={library}
+      ItemSeparatorComponent={ItemDivider}
       renderItem={({ item: track }) => (
         <TrackListItem
           track={{
