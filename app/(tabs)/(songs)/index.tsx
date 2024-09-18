4abb6b0fbdf4ec714e47b5f0bc833452e5c0503f
Leah Deshar <leah.desar@gmail.com>
2024-09-18 16:45:19 +0545

    navigation hook

--- app/(tabs)/(songs)/index.tsx
+++ app/(tabs)/(songs)/index.tsx
@@ -2,11 +2,17 @@ import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { defaultStyles } from "@/styles";
 import TrackList from "@/components/TrackList";
+import { screenPadding } from "@/constants/tokens";
 
 const SongsScreen = () => {
   return (
     <View style={defaultStyles.container}>
-      <ScrollView>
+      <ScrollView
+        style={{
+          paddingHorizontal: screenPadding.horizontal,
+        }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
         <TrackList scrollEnabled={false} />
       </ScrollView>
     </View>
