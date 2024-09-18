9a4afc001d5a246a58894debd7289f51b7f3222e
Leah Deshar <leah.desar@gmail.com>
2024-09-18 16:14:40 +0545

    bug fix

--- components/TrackList.tsx
+++ components/TrackList.tsx
@@ -1,7 +1,6 @@
 import { View, Text, FlatList, FlatListProps } from "react-native";
 import library from "@/assets/data/library.json";
 import TrackListItem from "./TrackListItem";
-// import { TrackListItem } from "./TrackListItem";
 
 export type TrackListProps = Partial<FlatListProps<unknown>>;
 const TrackList = ({ ...flatlistProps }: TrackListProps) => {
--- components/TrackListItem.tsx
+++ components/TrackListItem.tsx
@@ -1,7 +1,96 @@
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
-import React from "react";
-import FastImage from "react-native-fast-image";
+// import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+// import React from "react";
+// // import FastImage from "react-native-fast-image";
+// import { unknownTrackImageUri } from "@/constants/images";
+// import { colors, fontSize } from "@/constants/tokens";
+// import { defaultStyles } from "@/styles";
 
+// export type TrackListItemProps = {
+//   track: {
+//     title: string;
+//     image?: string;
+//     artist?: string;
+//   };
+// };
+
+// const TrackListItem = ({ track }: TrackListItemProps) => {
+//   const isActiveTrack = false;
+//   return (
+//     <TouchableHighlight>
+//       <View style={styles.trackItemContainer}>
+//         <View>
+//           <View>
+//             {/* <FastImage
+//               source={{
+//                 uri: track.image ?? unknownTrackImageUri,
+//                 priority: FastImage.priority.normal,
+//               }}
+//               style={{
+//                 ...styles.trackArtworkImage,
+//                 opacity: isActiveTrack ? 0.6 : 1,
+//               }}
+//             /> */}
+//           </View>
+//           <View
+//             style={{
+//               width: "100%",
+//             }}
+//           >
+//             <Text
+//               numberOfLines={1}
+//               style={{
+//                 ...styles.trackTitleText,
+//                 color: isActiveTrack ? colors.primary : colors.text,
+//               }}
+//             >
+//               {track.title}
+//             </Text>
+//             {track.artist && (
+//               <Text numberOfLines={1} style={styles.trackArtistText}>
+//                 {track.artist}
+//               </Text>
+//             )}
+//           </View>
+//         </View>
+//       </View>
+//     </TouchableHighlight>
+//   );
+// };
+
+// export default TrackListItem;
+
+// const styles = StyleSheet.create({
+//   trackItemContainer: {
+//     flexDirection: "row",
+//     columnGap: 14,
+//     alignItems: "center",
+//     paddingRight: 20,
+//   },
+//   trackArtworkImage: {
+//     width: 50,
+//     height: 50,
+//     borderRadius: 8,
+//   },
+//   trackTitleText: {
+//     ...defaultStyles.text,
+//     fontSize: fontSize.sm,
+//     fontWeight: "600",
+//     maxWidth: "90%",
+//   },
+//   trackArtistText: {
+//     ...defaultStyles.text,
+//     fontSize: fontSize.xs,
+//     color: colors.textMuted,
+//   },
+// });
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  Image,
+} from "react-native";
+import React from "react";
 import { unknownTrackImageUri } from "@/constants/images";
 import { colors, fontSize } from "@/constants/tokens";
 import { defaultStyles } from "@/styles";
@@ -19,39 +108,38 @@ const TrackListItem = ({ track }: TrackListItemProps) => {
   return (
     <TouchableHighlight>
       <View style={styles.trackItemContainer}>
+        {/* <View> */}
         <View>
-          <View>
-            <FastImage
-              source={{
-                uri: track.image ?? unknownTrackImageUri,
-                priority: FastImage.priority.normal,
-              }}
-              style={{
-                ...styles.trackArtworkImage,
-                opacity: isActiveTrack ? 0.6 : 1,
-              }}
-            />
-          </View>
-          <View
+          <Image
+            source={{
+              uri: track.image ?? unknownTrackImageUri,
+            }}
             style={{
-              width: "100%",
+              ...styles.trackArtworkImage,
+              opacity: isActiveTrack ? 0.6 : 1,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            width: "100%",
+          }}
+        >
+          <Text
+            numberOfLines={1}
+            style={{
+              ...styles.trackTitleText,
+              color: isActiveTrack ? colors.primary : colors.text,
             }}
           >
-            <Text
-              numberOfLines={1}
-              style={{
-                ...styles.trackTitleText,
-                color: isActiveTrack ? colors.primary : colors.text,
-              }}
-            >
-              {track.title}
+            {track.title}
+          </Text>
+          {track.artist && (
+            <Text numberOfLines={1} style={styles.trackArtistText}>
+              {track.artist}
             </Text>
-            {track.artist && (
-              <Text numberOfLines={1} style={styles.trackArtistText}>
-                {track.artist}
-              </Text>
-            )}
-          </View>
+          )}
+          {/* </View> */}
         </View>
       </View>
     </TouchableHighlight>
